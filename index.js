d3e9a4ff4dcddbdedd4b3c6eb0c31e5b47bccb09
Corey Colligan <coreyleec91288@gmail.com>
2021-03-26 16:21:46 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    })
+
+    // input.addEventListener('click', clickAlert);
+
 }
+
+
+// function() {
+//     alert('I was clicked!');
+//   }
